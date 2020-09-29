209be3433640ef76e7e222b6f28df450e4bb4092
cote87 <cote.jll@gmail.com>
2020-09-29 14:19:01 -0300

    Se agregan los archivos iniciales

---
+++ src/matematica/main/App.java
@@ -0,0 +1,11 @@
+package matematica.main;
+
+import matematica.control.Calculos;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println(Calculos.sumar(2, 2));
+	}
+
+}
